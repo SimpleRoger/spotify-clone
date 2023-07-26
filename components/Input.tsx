1d9df5dd99c27770b591bfab5f47b13c320e9533
Roger Tan <rtan4955@uni.sydney.edu.au>
2023-07-26 16:42:58 +1000

    final

---
+++ components/Input.tsx
@@ -0,0 +1,25 @@
+import { forwardRef } from "react";
+import { twMerge } from "tailwind-merge";
+
+interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {}
+
+const Input = forwardRef<HTMLInputElement, InputProps>(
+  ({ className, type, disabled, ...props }, ref) => {
+    return (
+      <input
+        type={type}
+        className={twMerge(
+          "flex w-full rounded-md bg-neutral-700 border border-transparent py-3 px-3 text-sm file:border-0 file:text-sm file:bg-transparent file:font-medium placeholder:text-neutral-400 disable:cursor-not-allowed disabled:opacity-50 focus:outline-none",
+          className
+        )}
+        disabled={disabled}
+        ref={ref}
+        {...props}
+      />
+    );
+  }
+);
+
+Input.displayName = "Input";
+
+export default Input;
---
+++ components/ListItem.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+import { FaPlay } from "react-icons/fa";
+
+interface ListItemProps {
+  image: string;
+  name: string;
+  href: string;
+}
+
+const ListItem: React.FC<ListItemProps> = ({ image, name, href }) => {
+  const router = useRouter();
+  const onClick = () => {
+    router.push(href);
+  };
+  //48:41
+  return (
+      <button
+        onClick={onClick}
+        className="relative
+    group flex items-center rounded-md overflow-hidden gap-x-4 bg-neutral-100/10 hover:bg-neutral-100/20 transition pr-4"
+      >
+        <div
+          className="relative
+        min-h[64px]
+        min-w-[64px]"
+        >
+          <Image className="object-cover" src={image} fill alt="Image" />
+        </div>
+        <p className="font-medium truncate py-5">{name}</p>
+        <div
+          className="absolute transiton opacity-0 rounded-full flex items-center justify-center
+         bg-green-500 p-4 right-5  dropshadow-md group-hover:opacity-100 hover:scale-110"
+        >
+          <FaPlay className="text-black" />
+        </div>
+      </button>
+  );
+};
+
+export default ListItem;
